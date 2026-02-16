80617c0943f80d40bc4b1242d681b33701a371c7
BLDolph <danacecurov@gmail.com>
2026-02-16 18:00:22 +0300

    Колонизация Марса

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def get():
+    return 'Миссия Колонизация Марса'
+
+
+@app.route('/index')
+def get_index():
+    return 'И на Марсе будут яблони цвести!'
+
+
+if __name__ == '__main__':
+    app.run('127.0.0.1', 8080)
